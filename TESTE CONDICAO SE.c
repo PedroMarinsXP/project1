067bdea2752f71674188b5bdf67a90add0ecfbf6
Pedro Nunes <pedro.nunes.fute10@gmail.com>
2022-06-22 10:52:14 -0300

    Primeiro commit - pj1

---
+++ TESTE CONDICAO SE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int hora;
+
+    printf("Digite a hora exata: ");
+    scanf("%d", &hora);
+
+    if (hora < 12){
+        pintf("Bom dia!\n");
+    }
+
+    else{
+        printf("Boa noite!\n");
+    }
+
+    return 0;
+}
+
